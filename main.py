2b90a6a6571e1c7709ffe8c7ad44f017e0e435f7
Stushbush <98846688+Stushbush@users.noreply.github.com>
2022-02-11 20:01:50 +0000

    Week-2-Challenge 1*1

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-Name=input("What is your name? ")
-print(f"Your name is {Name} ")
-First_Name=input("What is your first name? ")
-print(f"Your initial is {First_Name[0]}")
-Last_Name=input("What is your Last Name? ")
-print(f"Your surname initial is {Last_Name[0]}")
-print(f"Your initials are {First_Name[0], Last_Name[0]}")
+Scores=int(input("What is your score? "))
+print(f"Your grade is {Scores}")
+# grade_A=input("[90-100]")
+# for number in {grade_A}:
+#   input=("A")
+# print(f"{Scores}")
+if Scores >=90:
+  print("You received an A")
+if Scores >=80 and Scores <=89:
+  print ("You received a B")
+if Scores >=70 and Scores<=79:
+  print ("You received a C")
+if Scores >=60 and Scores<=69:
+  print("You received a D")
+if Scores<60:
+  print("You received a F")
+
 
