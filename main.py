96b987eab7e9abdd6e0efc78fad2d574392e9323
Stushbush <98846688+Stushbush@users.noreply.github.com>
2022-02-11 16:40:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+Name=input("What is your name? ")
+print(f"Your name is {Name} ")
+First_Name=input("What is your first name? ")
+print(f"Your initial is {First_Name[0]}")
+Last_Name=input("What is your Last Name? ")
+print(f"Your surname initial is {Last_Name[0]}")
+print(f"Your initials are {First_Name[0], Last_Name[0]}")
+
